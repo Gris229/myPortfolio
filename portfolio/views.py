7f0c5ec40fd780e2aeb544b9db40421e4abe06c4
grisdev <gehunelgbedji@gmail.com>
2025-11-24 19:13:35 +0100

    firstcommit

---
+++ portfolio/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def home(request):
+
+    return render(request, 'portfolio/home.html')
+
+def project(request):
+
+    return render(request, 'portfolio/project.html')
